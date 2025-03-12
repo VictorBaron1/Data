44272ca238b1e651c9f2ae4f2b630a3c771d695d
VictorBaron1 <victorb104@nycstudents.net>
2025-03-12 13:28:26 +0000

    first commit

---
+++ Data.java
@@ -0,0 +1,21 @@
+public class Data
+{
+public static final int MAX = (int) (Math.random() * Integer.MAX_VALUE);
+private int[][] grid;
+/** Fills all elements of grid with randomly generated values, as described in part (a)
+* Precondition: grid is not null.
+* grid has at least one element.
+*/
+public void repopulate()
+{ /* to be implemented in part (a) */ }
+/** Returns the number of columns in grid that are in increasing order, as described
+* in part (b)
+* Precondition: grid is not null.
+* grid has at least one element.
+*/
+public int countIncreasingCols()
+{ 
+    return 0;
+ }
+// There may be instance variables, constructors, and methods that are not shown.
+}
