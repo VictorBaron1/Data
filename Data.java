fd362fc12b907d853cb4d54fa45fdfdf7c173e15
VictorBaron1 <victorb104@nycstudents.net>
2025-03-18 13:09:42 +0000

    final

--- Data.java
+++ Data.java
@@ -1,9 +1,22 @@
 public class Data
 {
 public static final int MAX = (int) (Math.random() * Integer.MAX_VALUE);
-public Data(int rows, int cols)
+public Data(int[][] arr)
 {
-    grid = new int[rows][cols];
+    grid = arr;
+}
+public String toString()
+{
+    String s = "";
+    for(int[] row : grid)
+    {
+        for(int col : row)
+        {
+            s += col + " ";
+        }
+        s += "\n";
+    }
+    return s;
 }
 private int[][] grid;
 /** Fills all elements of grid with randomly generated values, as described in part (a)
@@ -11,12 +24,20 @@ private int[][] grid;
 * grid has at least one element.
 */
 public void repopulate()
-{ /* to be implemented in part (a) */ }
-/** Returns the number of columns in grid that are in increasing order, as described
-* in part (b)
-* Precondition: grid is not null.
-* grid has at least one element.
-*/
+{ 
+    for(int row = 0; row < grid.length; row++)
+    {
+        for(int col = 0; col < grid[0].length; col++)
+        {
+            int val = 0;
+            while( val % 10 != 0 || val % 100 ==0)
+            {
+                val = (int) (Math.random() * MAX + 1);
+            }
+            grid[row][col] = val; 
+        }
+    }
+}
 public int countIncreasingCols()
 { 
     return 0;
--- Main.java
+++ Main.java
@@ -4,5 +4,15 @@ public class Main
     public static void main(String[]args)
     {
         System.out.println(Data.MAX);
+        Data d = new Data[7][5];
+        System.out.println(d);
+        d.repopulate();
+        System.out.println(d);
+        int grid[][] = {
+            {10,50,40},
+            {20,40,20},
+            {30,50,30}
+        };
+        d = new Data(grid);
     }
 }
